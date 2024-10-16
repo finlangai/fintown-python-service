adf82330f1254a0bdd533cfcf63fd7c99ddb93ef
schmev <86550672+schmev91@users.noreply.github.com>
2024-10-16 11:48:55 +0700

    stash seeder

--- database/seeders/stash_seeder.py
+++ database/seeders/stash_seeder.py
@@ -1,9 +1,9 @@
 from app.utils import print_green_bold, model_mapper, json_camel_to_snake, text_to_red
-from app.services import StockFinanceService, FormularResolver
+from app.services import StockFinanceService, FormularResolver, StockQuoteService
 from app.enums import ParamLocation
 from config.seeder import STOCK_SYMBOLS
 from core import mongodb
-
+from datetime import timedelta, datetime
 import numpy as np, pandas as pd
 
 
@@ -19,40 +19,51 @@ def main():
     sum_equity = 0
     sum_revenue = 0
 
+    period_list = ["quarter", "year"]
     for symbol in vn30_list:
-        financeService.update_symbol(symbol=symbol)
+        company_stash = {"symbol": symbol, "quarter": [], "year": []}
 
-        ratio_df = financeService.get_data(ParamLocation.ratio)
+        for period in period_list:
+            financeService.update_period(period)
+            financeService.update_symbol(symbol=symbol)
 
-        income_df = financeService.get_data(ParamLocation.income_statement)
-        balance_df = financeService.get_data(ParamLocation.balance_sheet)
+            # get required df
+            ratio_df = financeService.get_data(ParamLocation.ratio)
+            income_df = financeService.get_data(ParamLocation.income_statement)
+            balance_df = financeService.get_data(ParamLocation.balance_sheet)
 
-        company_stash = {"symbol": symbol, "historical": []}
-        stash_df = financeService.get_meta_df().head()
+            stash_df = financeService.get_meta_df().head()
 
-        income_stash = income_df.head()[
-            [
-                "Net Profit For the Year",
-                "Revenue (Bn. VND)",
+            income_stash = income_df.head()[
+                [
+                    "Net Profit For the Year",
+                    "Revenue (Bn. VND)",
+                ]
             ]
-        ]
-        balance_stash = balance_df.head()[["OWNER'S EQUITY(Bn.VND)"]]
+            balance_stash = balance_df.head()[["OWNER'S EQUITY(Bn.VND)"]]
 
-        stash_df = pd.concat([stash_df, income_stash, balance_stash], axis=1)
-        # rename column for inserting
-        stash_df.rename(
-            columns={
+            # merge dataframes
+            stash_df = pd.concat([stash_df, income_stash, balance_stash], axis=1)
+            rename_dict = {
                 "yearReport": "year",
-                "lengthReport": "quarter",
                 "Net Profit For the Year": "net_profit",
                 "Revenue (Bn. VND)": "revenue",
                 "OWNER'S EQUITY(Bn.VND)": "equity",
-            },
-            inplace=True,
-        )
-        for _, row in stash_df.iterrows():
-            company_stash["historical"].append(row.to_dict())
+            }
+            # rename the quarter column if is quarter period
+            if period == "quarter":
+                rename_dict.update({"lengthReport": "quarter"})
 
+            # rename column for inserting
+            stash_df.rename(
+                columns=rename_dict,
+                inplace=True,
+            )
+
+            for _, row in stash_df.iterrows():
+                company_stash[period].append(row.to_dict())
+
+        # append the stash to holder for later insert
         stash_holder.append(company_stash)
         print_green_bold(f"generate stashed for {symbol}")
 
@@ -62,6 +73,26 @@ def main():
         sum_revenue += income_df.iloc[0]["Revenue (Bn. VND)"]
         sum_equity += balance_df.iloc[0]["OWNER'S EQUITY(Bn.VND)"]
 
+    # ======================================================
+    # ==================CALCULATE FOR V30===================
+    # ======================================================
+    # calculate 52w mean
+    quoteService = StockQuoteService()
+
+    # Get the current date
+    current_date = datetime.now().date()
+    # Format the current date as YYYY-MM-DD
+    today = current_date.strftime("%Y-%m-%d")
+    # Calculate the date 52 weeks before today
+    date_52_weeks_ago = current_date - timedelta(weeks=52)
+    # Format the date 52 weeks ago as YYYY-MM-DD
+    today_52w_before = date_52_weeks_ago.strftime("%Y-%m-%d")
+
+    weekly_df = quoteService.history(
+        start=today_52w_before, end=today, symbol="VN30", interval="1W"
+    ).dropna()
+    avg_52w = weekly_df["close"].tail(52).mean()
+
     pevn30 = sum_marketcap / sum_earnings
     pbvn30 = sum_marketcap / sum_equity
 
@@ -69,6 +100,7 @@ def main():
         "symbol": "VN30",
         "pe": float(pevn30),
         "pb": float(pbvn30),
+        "avg_52w": float(avg_52w),
         "marketcap": int(sum_marketcap),
         "earnings": int(sum_earnings),
         "revenue": int(sum_revenue),
